7fe58bcc585c6f5f7c8ea67caa069710c1ce98b8
Pablo Neira Ayuso <pablo@netfilter.org>
2022-01-04 12:17:11 +0100

    output: GPRINT: fix it with NFLOG
    Add ULOGD_DTYPE_RAW to GPRINT to make it work, it does not provide much
    information since raw packets come with only a few fields set on.
    
    Therefore, update example ulogd.conf.in file since BASE provides a more
    complete packet dissection.
    
    Fixes: 59a71256945d ("src: add example use of GPRINT to ulogd.conf.in configuration file")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- output/ulogd_output_GPRINT.c
+++ output/ulogd_output_GPRINT.c
@@ -249,7 +249,7 @@ static int gprint_fini(struct ulogd_pluginstance *pi)
 static struct ulogd_plugin gprint_plugin = {
 	.name = "GPRINT",
 	.input = {
-		.type = ULOGD_DTYPE_PACKET | ULOGD_DTYPE_FLOW | ULOGD_DTYPE_SUM,
+		.type = ULOGD_DTYPE_RAW | ULOGD_DTYPE_PACKET | ULOGD_DTYPE_FLOW | ULOGD_DTYPE_SUM,
 	},
 	.output = {
 		.type = ULOGD_DTYPE_SINK,
